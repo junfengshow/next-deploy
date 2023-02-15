c0ec8b2678c3335d1379cc52822559048880f944
wujunfeng <wujf@8btc.com>
2023-02-15 17:31:45 +0800

    fe

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const ModuleFederationPlugin =
-  require("webpack").container.ModuleFederationPlugin;
-
-const NextFederationPlugin = require("@module-federation/nextjs-mf").default;
 
 const withMDX = require("@next/mdx")({
   options: {
@@ -13,18 +9,6 @@ const withMDX = require("@next/mdx")({
 const nextConfig = withMDX({
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  webpack: (config, { webpack, dev }) => {
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: "federation",
-        filename: "static/testFederation.js",
-        exposes: {
-          "./Button": "./components/Button.tsx",
-        },
-      })
-    );
-    return config;
-  },
 });
 
 module.exports = nextConfig;
